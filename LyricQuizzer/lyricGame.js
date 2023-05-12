cb9decebef31270f95662688fdfbfe5c3ed29a91
Alan Liang <alanliang@live.com>
2023-05-12 13:52:01 -0500

    finished login id authroizations

--- LyricQuizzer/lyricGame.js
+++ LyricQuizzer/lyricGame.js
@@ -1,3 +1,23 @@
+var clientID = "";
+var clientSecret = "";
+var redirect_uri = "https://pixelfish123.github.io/LyricQuizzer/lyricGame.html";
+
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
 function pickPlaylist() {
     console.log("pickPlaylist");
+}
+
+function submitLogin() {
+    let clientID = document.getElementById("clientID").value;
+    let clientSecret = document.getElementById("secretID").value;
+    localStorage.setItem("clientID", clientID);
+    localStorage.setItem("clientSecret", clientSecret);
+    // location.href = "\lyricGame.html";
+    let url = AUTHORIZE;
+    url += "?client_id=" + clientID;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    url += "&scope=playlist-read-private user-library-read user-red-email";
+    window.location.href = url;
 }
