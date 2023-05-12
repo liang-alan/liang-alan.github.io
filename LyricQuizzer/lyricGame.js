12336fa89f5195fae6ba461a8e2837c15f803ade
Alan Liang <alanliang@live.com>
2023-05-12 14:41:07 -0500

    api calls should be ready?

--- LyricQuizzer/lyricGame.js
+++ LyricQuizzer/lyricGame.js
@@ -24,6 +24,7 @@ function onLoad() {
 }
 function handleRedirect() {
     let code = getCode();
+    fetchAccessToken(code);
 }
 
 function getCode() {
@@ -35,3 +36,42 @@ function getCode() {
     }
     return code;
 }
+
+function fetchAccessToken(code) {
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code;
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + clientID;
+    body += "&client_secret=" + clientSecret;
+    callAuthorizationApi(body);
+}
+
+function callAuthorizationApi(body) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://accounts.spotify.com/api/token", true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onLoad = handleAuthorizationResponse();
+}
+
+function handleAuthorizationResponse() {
+    if (this.status == 200) {
+        console.log(data);
+        var data = JSON.parse(this.responseText);
+    
+        if (data.access_token != undefined) {
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if (data.refresh_token != undefined) {
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    } else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+
