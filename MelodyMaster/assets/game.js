11b329276682070c452bb70922a0b80ee4f18022
Alan Liang <alanliang@live.com>
2023-11-13 16:34:34 -0600

    created falling note

--- MelodyMaster/assets/game.js
+++ MelodyMaster/assets/game.js
@@ -1,44 +1,52 @@
+var beat = 0;
+
 function onStart() {
-    
+    alert("Are you ready to start?" +
+        "\nUse Q, W, E, I, O, P to play the notes." +
+        "\nWhen notes reach the corresponding button, press the key" +
+        "\nFor the best experience, use headphones");
+    // startTimer();
 }
 
-const A = "red";
-const Adown = "#cc0000";
-const B = "orangered";
-const Bdown = "#cc3700";
-const C = "goldenrod";
-const Cdown = "#ae8319";
-const D = "teal";
-const Ddown = "#004d4d";
-const E = "blueviolet";
-const Edown = "#701ac0";
-const F = "magenta";
-const Fdown = "#cc00cc";
+
+const Q = "red";
+const Qdown = "#cc0000";
+const W = "orangered";
+const Wdown = "#cc3700";
+const E = "goldenrod";
+const Edown = "#ae8319";
+const I = "teal";
+const Idown = "#004d4d";
+const O = "blueviolet";
+const Odown = "#701ac0";
+const P = "magenta";
+const Pdown = "#cc00cc";
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Q' || event.key === 'q') {
         // console.log('Q was pressed');
-        document.getElementById("buttonA").style.backgroundColor = Adown;
+        document.getElementById("buttonQ").style.backgroundColor = Qdown;
     }
     if (event.key === 'W' || event.key === 'w') {
         // console.log('W was pressed');
-        document.getElementById("buttonB").style.backgroundColor = Bdown;
+        document.getElementById("buttonW").style.backgroundColor = Wdown;
     }
     if (event.key === 'E' || event.key === 'e') {
         // console.log('E was pressed');
-        document.getElementById("buttonC").style.backgroundColor = Cdown;
+        document.getElementById("buttonE").style.backgroundColor = Edown;
     }
     if (event.key === 'I' || event.key === 'i') {
         // console.log('I was pressed');
-        document.getElementById("buttonD").style.backgroundColor = Ddown;
+        document.getElementById("buttonI").style.backgroundColor = Idown;
     }
     if (event.key === 'O' || event.key === 'o') {
         // console.log('O was pressed');
-        document.getElementById("buttonE").style.backgroundColor = Edown;
+        document.getElementById("buttonO").style.backgroundColor = Odown;
     }
     if (event.key === 'P' || event.key === 'p') {
         // console.log('P was pressed');
-        document.getElementById("buttonF").style.backgroundColor = Fdown;
+        console.log(parseInt(document.getElementById("score").innerHTML.split(":")[1]));
+        document.getElementById("buttonP").style.backgroundColor = Pdown; 
     }
 });
 
@@ -46,26 +54,26 @@ document.addEventListener('keydown', function (event) {
 document.addEventListener('keyup', function (event) {
     if (event.key === 'Q' || event.key === 'q') {
         // console.log('Q was released');
-        document.getElementById("buttonA").style.backgroundColor = A;
+        document.getElementById("buttonQ").style.backgroundColor = Q;
     }
     if (event.key === 'W' || event.key === 'w') {
         // console.log('W was released');
-        document.getElementById("buttonB").style.backgroundColor = B;
+        document.getElementById("buttonW").style.backgroundColor = W;
     }
     if (event.key === 'E' || event.key === 'e') {
         // console.log('E was released');
-        document.getElementById("buttonC").style.backgroundColor = C;
+        document.getElementById("buttonE").style.backgroundColor = E;
     }
     if (event.key === 'I' || event.key === 'i') {
         // console.log('I was released');
-        document.getElementById("buttonD").style.backgroundColor = D;
+        document.getElementById("buttonI").style.backgroundColor = I;
     }
     if (event.key === 'O' || event.key === 'o') {
         // console.log('O was released');
-        document.getElementById("buttonE").style.backgroundColor = E;
+        document.getElementById("buttonO").style.backgroundColor = O;
     }
     if (event.key === 'P' || event.key === 'p') {
         // console.log('P was released');
-        document.getElementById("buttonF").style.backgroundColor = F;
+        document.getElementById("buttonP").style.backgroundColor = P;
     }
 });
---
+++ MelodyMaster/game-scripts/song1.js
@@ -0,0 +1,94 @@
+function startTimer() {
+    createNotes();
+    var min = 0;
+    var sec = 0;
+    setInterval(function () {
+        // console.log((beat % 4) + 1);
+        beat++;
+        // document.getElementById("beat").innerHTML = beat;
+    }, 750);
+    setInterval(function () {
+        if (sec == 60) {
+            min++;
+            sec = 0;
+        }
+        document.getElementById("timer").innerHTML = min;
+        if (sec < 10) {
+            document.getElementById("timer").innerHTML += ":0" + sec;
+        } else {
+            document.getElementById("timer").innerHTML = min + ":" + sec;
+        }
+        sec++;  // increment seconds
+    }, 1000);
+
+}
+function createNotes() {
+    spawn("P");
+
+
+    function spawn(letter) {
+        // console.log("spawning", "button"+ letter);
+        var button = document.getElementById("button" + letter);
+        var x = button.getBoundingClientRect().left;
+        // x += button.offsetWidth / 2;
+        var y = 0.1 * window.innerHeight;
+        var note = document.createElement("div");
+        note.className = 'note color6 circle'
+        note.style.width = button.getBoundingClientRect().width + "px";
+        note.style.height = note.style.width;
+        note.style.left = x + "px";
+        note.style.top = y + "px";
+        note.addEventListener('animationend', handleAnimationEnd);
+        // note.addEventListener('keydown', function (event) {
+        //     if (event.key === letter || event.key === letter.toLowerCase()) {
+        //         console.log("hello!")
+        //         if (checkOverlap(button, note)) {
+        //             note.remove();
+        //             document.getElementById("score").innerHTML = "Score:" + (parseInt(document.getElementById("score").innerHTML.split(":")[1]) + 1);
+
+        //         }
+        //     }
+        // });
+        document.getElementById("col"+letter).appendChild(note);
+        // var p = document.createElement("p");
+        // var text = document.createTextNode("Hello World");
+        // p.appendChild(text);
+        // document.getElementById("game").appendChild(p);
+        console.log("spawned", letter);
+    }
+    function handleAnimationEnd(event) {
+        // Remove the animated element from the DOM
+        event.target.remove();
+    }
+    function checkOverlap(button, note) {
+        var noteRect = note.getBoundingClientRect();
+        var buttonRect = button.getBoundingClientRect();
+        var confidence = buttonRect.width * 0.1;
+
+        if (
+            noteRect.bottom > buttonRect.bottom - confidence ||
+            noteRect.bottom < buttonRect.bottom + confidence
+        ) {
+            console.log("Circle is on top of the button");
+            noteRect.remove(); // Remove the circle if it overlaps with the button
+        }
+    }
+}
+
+// Get the falling circle element
+var fallingCircle = document.getElementsByClassName('note color6');
+// Set the initial position of the falling circle
+var positionY = 0;
+// Set the animation speed (you can adjust this)
+var speed = 2;
+// Function to update the position of the falling circle
+function updatePosition() {
+    // Increment the Y position
+    positionY += speed;
+
+    // Apply the new position
+    fallingCircle.style.top = positionY + 'px';
+
+    // Request the next animation frame
+    requestAnimationFrame(updatePosition);
+}
