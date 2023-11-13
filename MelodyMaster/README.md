100e07010a12123c01da6a9f10cd957aa987dcee
Alan Liang <alanliang@live.com>
2023-11-12 20:37:07 -0600

    Started Melody Master homepage and setup CSS/fonts

---
+++ MelodyMaster/README.md
@@ -0,0 +1,8 @@
+# Music Tiles project
+Last updated: 11/12/2023
+
+This is a project I am working on for fun.
+
+I've always had a passion for video games as well as music so I wanted to test out my abilities to build a small game from scratch.
+
+Credits:
---
+++ MelodyMaster/assets/main.js
@@ -0,0 +1,28 @@
+function handleClick(buttonId) {
+    switch (buttonId) {
+        case 1:
+            alert("Button 1 clicked");
+            break;
+        case 2:
+            alert("Button 2 clicked");
+            break;
+        case 3:
+            alert("Button 3 clicked");
+            break;
+        case 4:
+            alert("Button 4 clicked");
+            break;
+        case 5:
+            alert("Button 5 clicked");
+            break;
+        case 6:
+            alert("Button 6 clicked");
+            break;
+        case 7:
+            alert("Button 7 clicked");
+            break;
+        case 8:
+            alert("Button 8 clicked");
+            break;
+    }
+}
